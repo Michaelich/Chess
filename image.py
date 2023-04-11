86713181fd5c3a1d44b384aad2d3c229383c1245
Michał Beń <be.micha.mb@gmail.com>
2023-04-11 11:28:29 +0200

    Small changes
    Put pictures into folder, small cosmetic changes in code

--- image.py
+++ image.py
@@ -1,18 +1,18 @@
 import pygame
 images = []
 images2 = []
-image1 = pygame.image.load("white_pawn.png")
-image2 = pygame.image.load("black_pawn.png")
-image3 = pygame.image.load("white_rook.png")
-image4 = pygame.image.load("black_rook.png")
-image5 = pygame.image.load("white_king.png")
-image6 = pygame.image.load("black_king.png")
-image7 = pygame.image.load("white_knight.png")
-image8 = pygame.image.load("black_knight.png")
-image9 = pygame.image.load("white_bishop.png")
-image10 = pygame.image.load("black_bishop.png")
-image11 = pygame.image.load("white_queen.png")
-image12 = pygame.image.load("black_queen.png")
+image1 = pygame.image.load("Figures/white_pawn.png")
+image2 = pygame.image.load("Figures/black_pawn.png")
+image3 = pygame.image.load("Figures/white_rook.png")
+image4 = pygame.image.load("Figures/black_rook.png")
+image5 = pygame.image.load("Figures/white_king.png")
+image6 = pygame.image.load("Figures/black_king.png")
+image7 = pygame.image.load("Figures/white_knight.png")
+image8 = pygame.image.load("Figures/black_knight.png")
+image9 = pygame.image.load("Figures/white_bishop.png")
+image10 = pygame.image.load("Figures/black_bishop.png")
+image11 = pygame.image.load("Figures/white_queen.png")
+image12 = pygame.image.load("Figures/black_queen.png")
 images=[image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12]
 for i in range(len(images)):
     images[i].set_colorkey((0, 0, 0))
